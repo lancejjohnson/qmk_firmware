53a453c835ae9d5a9911f71c98d32100dd0a41d8
Lance Johnson <lancejjohnson@gmail.com>
2021-07-18 17:21:56 -0400

    Fix up and down vim mappings

--- keyboards/preonic/keymaps/lancejjohnson/keymap.c
+++ keyboards/preonic/keymaps/lancejjohnson/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,   KC_Y,   KC_U,  KC_I,    KC_O,   KC_P,    KC_BSLS,
   LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,  KC_G,   KC_H,   KC_J,  KC_K,    KC_L,   KC_SCLN, KC_QUOT,
   KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,   KC_N,   KC_M,  KC_COMM, KC_DOT, KC_SLSH, KC_SFTENT,
-  BACKLIT,        KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_SPC, KC_SPC, RAISE, KC_LEFT, KC_UP,  KC_DOWN, KC_RGHT
+  BACKLIT,        KC_LCTL, KC_LALT, KC_LGUI, LOWER, KC_SPC, KC_SPC, RAISE, KC_LEFT, KC_DOWN,  KC_UP, KC_RGHT
 ),
 
 /* Lower
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______, KC_UNDS, _______, KC_LCBR, KC_RCBR, KC_DEL,
   KC_DEL,  _______, _______, _______, _______, _______, _______, KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_PIPE,
   _______, _______, _______, _______, _______, _______, _______, KC_PPLS, _______, _______, _______, _______,
-  _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END
+  _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
 
 /* Raise
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_preonic_grid(
   KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,      KC_F11,
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,     KC_0,        KC_DEL,
-  KC_DEL,  _______, _______, _______, _______, _______, KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT, KC_RBRACKET, KC_BSLASH,
+  KC_DEL,  _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN,   KC_UP, KC_RIGHT, KC_RBRACKET, KC_BSLASH,
   _______, _______, _______, _______, _______, _______, _______, KC_NUHS, KC_NUBS, KC_PGUP,  KC_PGDN,     _______,
   _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLU,  KC_VOLD,     KC_MPLY
 ),
